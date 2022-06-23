fdc3c4ace4a882499db7776ef35f39e7dce1faf3
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-06-23 21:59:57 +0500

    add drop menu

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,10 @@ const openMenuButton = document.querySelector('.bulletins__button-open');
 const closeMenuButton = document.querySelector('.bulletins__button-close');
 const text = document.querySelector('.bulletins__description');
 const image = document.querySelector('.bulletins__bottom-image');
+
+const dropMenu = document.querySelector('.drop-menu');
+const openDropMenuButton = document.querySelector('.header__burger-open');
+const closeDropMenuButton = document.querySelector('.header__burger-close');
  
 openMenuButton.addEventListener('click', () => { 
   menu.classList.add('content__bulletins-without-button_open')
@@ -18,6 +22,18 @@ closeMenuButton.addEventListener('click', () => {
   openMenuButton.classList.add('bulletins__button_open')
   text.classList.add('bulletins__description_open')
   image.classList.remove('bulletins__bottom-image_open')
+}); 
+
+openDropMenuButton.addEventListener('click', () => { 
+  dropMenu.classList.add('drop-menu_open')
+  openDropMenuButton.classList.remove('header__burger_open')
+  closeDropMenuButton.classList.add('header__burger_open')
+}); 
+
+closeDropMenuButton.addEventListener('click', () => { 
+  dropMenu.classList.remove('drop-menu_open')
+  closeDropMenuButton.classList.remove('header__burger_open')
+  openDropMenuButton.classList.add('header__burger_open')
 }); 
 
 
